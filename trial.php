6949148c95292edf517f42621c2fbaa3154b8fc9
anirjoshi <45450979+anirjoshi@users.noreply.github.com>
2018-12-01 17:05:39 +0530

    Add files via upload

--- trial.php
+++ trial.php
@@ -1,8 +1,79 @@
 <html>
-  <head>
+<body>
+<form action="trial.php" method="post" enctype="multipart/form-data">
+		Type The Word: <br>
+		 <input type="text" name="v1">
+		<br>
+		Hindi To English <input type="radio" name="buttongroup1" value="1" id="cb1">
+		<br>
+
+		English To Hindi <input type="radio" name="buttongroup1" id="cb2" value="2">
+	
+		<br>
+
+		<input type="submit" value="Go!"> 
+	</form>
+	
+<head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
-    <script src="https://www.google.com/jsapi">
-    </script>
+    	<script src="https://www.google.com/jsapi"></script>
+</head>
+
+
+<?php
+$notable=0;
+if(isset($_POST["Go!"]))
+	$notable=1;
+$servername = "localhost";
+$username = "root";
+$password = "ani";
+//$dbname = "nlp";
+$conn = mysqli_connect($servername, $username, $password,"nlp");
+if (!$conn)
+	echo "Connection failed<br>";
+else
+	echo "Connection Established with nlp<br>";
+
+$x = $_POST["v1"];
+
+if($_POST['buttongroup1']=='1'){
+	$selection_of_word = "SELECT english FROM words WHERE hindi='$x'";
+	//echo $selection_of_word." here<br>";
+	$result=mysqli_query($conn,$selection_of_word);
+	while($row = mysqli_fetch_row($result))	{
+		$p=$p."<tr><td>". $row[0]."</td><td>".$x."</td></tr>";
+		
+	}
+	if($notable==1){
+	echo "<table border='2'>
+			<tr>
+				<th>English</th>
+				<th>Hindi</th>
+			</tr>
+				$p
+			</table>	";
+	}
+}
+
+if($_POST['buttongroup1']=='2'){
+	$selection_of_word = "SELECT hindi FROM words WHERE english='$x'";
+	//echo $selection_of_word." here<br>";
+	$result=mysqli_query($conn,$selection_of_word);
+	while($row = mysqli_fetch_row($result))	{
+		$p=$p."<tr><td>".$x."</td><td>".$row[0]."</td></tr>";
+		$eng_hin=$row[0];
+	}
+	echo "<table border='2'>
+			<tr>
+				<th>English</th>
+				<th>Hindi</th>
+			</tr>
+				$p
+			</table>	";
+}
+
+?>
+
     <script>
 		google.load("elements", "1", {packages: "transliteration"});
 
@@ -32,13 +103,17 @@
       	if(code == 13) 
       	{ 
           setTimeout(myFunction, 300)
-	  }
+	  		}
 	}
-    </script>
-  </head>
+	</script>
   <body>
     Type in Hindi (Press Ctrl+g to toggle between English and Hindi)<br>
-    <textarea id="transliterateTextarea" onKeyPress="enterpressalert(event, this)"></textarea>
+    <textarea id="transliterateTextarea" onKeyPress="enterpressalert(event, this)"><?php
+    	if($_POST['buttongroup1']=='1')
+    		echo $_POST["v1"];
+    	if($_POST['buttongroup1']=='2')
+    		echo $eng_hin;
+    ?></textarea>
     <p id="https"></p>
   </body>
 </html> 
